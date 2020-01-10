5b23adeb948e71dd455e8fc27e6170e020c5f9a4
liupu14 <lpgauss@163.com>
2020-01-10 23:11:18 +0800

    adjust jiugonggePlot to add gridline's selection

--- SpecialPlot.py
+++ SpecialPlot.py
@@ -6,18 +6,26 @@ import seaborn as sns
 sns.set(style="ticks")
 plt.rcParams['font.sans_serif'] = ['SimHei']
 
-def jiugonggePlot(x,y,color,namelabel,df):
+def jiugonggePlot(x,y,color,namelabel,ways="abs",df):
     x = df['x']
     y = df['y']
     color = df['color']
     namelabel = df['namelabel']
-    sns.relplot(x=x,y=y,hue=color,deta=df)
+    x_min = np.min(x)
+    x_max = np.max(x)
+    y_min = np.min(y)
+    y_max = np.max(y)
+    x_ld = (x_min + (x_max - x_min)/3) if ways=="abs" else np.percentile(x,33.3)
+    x_ud = (x_max - (x_max - x_min)/3) if ways=="abs" else np.percentile(x,66.7)
+    y_ld = (y_min + (y_max - y_min)/3) if ways=="abs" else np.percentile(y,33.3)
+    y_ud = (y_min + (y_max - y_min)/3) if ways=="abs" else np.percentile(y,66.7)
+    sns.relplot(x=x,y=y,hue=color,data=df)
     for ii,jj,zz in zip(x,y,namelabel):
         plt.text(ii,jj,zz,fontsize=8)
-    plt.hlines(y=np.percentile(y,33.3),xmin=np.min(x),xmax=np.max(x),colors="red")
-    plt.hlines(y=np.percentile(y,66.7),xmin=np.min(x),xmax=np.max(x),colors="red")
-    plt.vlines(x=np.percentile(x,33.3),ymin=np.min(y),ymax=np.max(y),colors="red")
-    plt.vlines(x=np.percentile(x,66.7),ymin=np.min(y),ymax=np.max(y),colors="red")
+    plt.hlines(y=y_ld,xmin=x_min,xmax=x_max,colors="red")
+    plt.hlines(y=y_ud,xmin=x_min,xmax=x_max,colors="red")
+    plt.vlines(x=x_ld,ymin=y_min,ymax=y_max,colors="red")
+    plt.vlines(x=x_ld,ymin=y_min,ymax=y_max,colors="red")
     plt.show()
 
     
